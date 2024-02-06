c16be21f49fc6c27aed2bd67863753344cff0f71
Priyanshu Kumar <99123745+priyanshu69code@users.noreply.github.com>
2024-02-06 17:08:11 +0530

    Created the models for the review

--- review/models.py
+++ review/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
+from core import models as core_model
 
 # Create your models here.
+
+
+class Review(core_model.TimeStamps):
+    review = models.CharField(max_length=1000)
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    clearniness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.review
--- rooms/admin.py
+++ rooms/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from rooms.models import Room
+from rooms.models import Room, HouseRule, Amenities, Facility, RoomType
 # Register your models here.
 
 
 admin.site.register(Room)
+admin.site.register(HouseRule)
+admin.site.register(Facility)
+admin.site.register(Amenities)
+admin.site.register(RoomType)
