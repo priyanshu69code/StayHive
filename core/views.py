103bf61f01f7f4280014b499a08a924c1fda0136
Priyanshu Kumar <99123745+priyanshu69code@users.noreply.github.com>
2024-03-06 00:57:38 +0530

    Created the pagignated home page setup the templates also

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.core.paginator import Paginator
+from rooms.models import Room
 # Create your views here.
+
+
+def homepage(request):
+    page = request.GET.get("page")
+    room_list = Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    return render(request, "core/home.html", {"rooms": rooms})
