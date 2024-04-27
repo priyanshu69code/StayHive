7ed00f46fe7671b1f810536e6f0d5e1cc3ad0850
Priyanshu Kumar <99123745+priyanshu69code@users.noreply.github.com>
2024-04-27 19:53:47 +0530

    Add whitenoise middleware to MIDDLEWARE in settings.py, Add serve view for media files in urls.py, Add static files serving in DEBUG mode in urls.py

--- config/settings.py
+++ config/settings.py
@@ -71,6 +71,7 @@ INTERNAL_IPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
